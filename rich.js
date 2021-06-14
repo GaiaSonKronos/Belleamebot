462e3681531513dac23baf42e270d881e967328b
GaiaSonKronos <85841094+GaiaSonKronos@users.noreply.github.com>
2021-06-14 18:29:11 +0530

    Update rich.js

--- rich.js
+++ rich.js
@@ -1,19 +1,11 @@
-// This template was made by FusionTerror.
-const DiscordRPC = require('discord-rpc'); // Requiring the discord-rpc package.
-const client = new DiscordRPC.Client({ transport: 'ipc' }); // Creating a client variable with is our rpc client.
-require('dotenv').config(); // Requiring the content of our .env file.
-
-(async () => {
-    client.on('ready', async () => { // Calling the ready event.
-        await client.setActivity({ // Setting the Rich Presence Activity based on what is passed in here.
-            buttons: [{ label: "Button Title Here", url: "Link For Button Here" }],
-            details: "Description",
-            largeImageKey: "rpc_icon",
-            largeImageText: "This text is shown when hovering over the largeImageKey in Discord."
-        }).catch(err => console.log(err));
-
-        console.log("Discord Rich Presence has been enabled.");
-    });
-
-    await client.login({ clientId: process.env.applicationID }).catch(console.error); // Logging into our application.
-})();
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Main Kuch Nai Karta";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "mast";
+    discordPresence.partyId = "1";
+    Discord_UpdatePresence(&discordPresence);
+}
